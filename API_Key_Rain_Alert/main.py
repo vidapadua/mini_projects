ba609b3e09e5a9b9411bfc810704fd4a0905a054
vidar <vidapadu@gmail.com>
2025-03-21 10:16:27 -0600

    MainComputerDK/CA

--- API_Key_Rain_Alert/main.py
+++ API_Key_Rain_Alert/main.py
@@ -1,22 +1,24 @@
 import requests
+from apikey import get_user_api_key
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 
 calgary_param={
     "lat":51.0460954,
     "lon":-114.065465,
-    "appid":"5111f6215a3a39240438afde46df30a3" #API Key Generated
+    "appid": get_user_api_key() #API Key Generated
 }
 
 odense_param={
     "lat":55.3997225,
     "lon":10.3852104,
-    "appid":"5111f6215a3a39240438afde46df30a3"
+    "appid": get_user_api_key()
 }
 
 
 ca_response=requests.get(OWM_Endpoint, params=calgary_param)
 ca_data=ca_response.json()
+print(ca_data)
 
 dk_response=requests.get(OWM_Endpoint, params=odense_param)
 dk_data=dk_response.json()
