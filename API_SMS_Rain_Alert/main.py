87eac76d3e17cd13c129d0eeb3a0e61c13714102
vidar <vidapadu@gmail.com>
2025-03-24 03:18:27 -0600

    MainComputerDK/CA

---
+++ API_SMS_Rain_Alert/main.py
@@ -0,0 +1,42 @@
+import requests
+from twilio.rest import Client
+from apikey import *
+from parameters import *
+
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+
+def bring_umbrella(param,city_name):
+    response = requests.get(OWM_Endpoint, params=param)
+    data = response.json()
+    # print(data["list"][0]["weather"][0]["id"])
+
+    will_rain = False
+    for hour_data in data["list"]:
+        # print checks:
+        # print(hour_data["dt_txt"])
+        # print (condition_code)
+        condition_code = hour_data["weather"][0]["id"]
+        if int(condition_code) < 700:
+            will_rain = True
+
+    print(f"In {city_name}:")
+    if will_rain:
+        print("Bring an umbrella today.\n")
+        print("SMS reminder sent to verified user.")
+        client = Client(get_user_account_sid(),get_user_auth_token())
+        message = client.messages.create(
+            body= "It's going to rain today. Remember to bring an umbrella!",
+            from_ = get_user_from_number(),
+            to= get_user_verified_calling_number()
+        )
+        # print(f"SMS sent: {message.sid}")
+
+    else:
+        print("It will not rain today according to the forecast.\n")
+        print("No SMS reminder message sent.")
+
+
+bring_umbrella(toronto_param,"Toronto, Canada")
+bring_umbrella(calgary_param, "Calgary,Canada")
+bring_umbrella(odense_param,"Odense, Denmark")
---
+++ API_SMS_Rain_Alert/parameters.py
@@ -0,0 +1,24 @@
+from apikey import get_user_api_key
+
+calgary_param={
+    "lat":51.0460954,
+    "lon":-114.065465,
+    "appid": get_user_api_key(), #API Key Generated
+    "cnt": 4
+}
+
+odense_param={
+    "lat":55.3997225,
+    "lon":10.3852104,
+    "appid": get_user_api_key(), #API Key Generated
+    "cnt": 4
+}
+
+toronto_param={
+    "lat":43.653225,
+    "lon":-79.383186,
+    "appid": get_user_api_key(), #API Key Generated
+    "cnt": 4
+}
+
+
